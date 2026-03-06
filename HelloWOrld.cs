3c22f8936a4c543af79b5db7b76d9fa0d7d46a87
olegariojr <pablopeon2012@gmail.com>
2026-03-06 18:52:01 +0100

    Add files via upload
    Mi primer script en unity

---
+++ HelloWOrld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWOrld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("keloke");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
